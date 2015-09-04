69f34c884ff075620ed2612272091d096d702407
Seth Tippetts <sethtippetts@gmail.com>
2015-09-04 16:18:04 -0600

    Moving files to `src` and compiling to `lib`

--- index.js
+++
@@ -1,5 +0,0 @@
-var waterline = require('./es5');
-if ('function' === typeof Map) waterline = require('./es6');
-
-module.exports = exports = waterline.default || waterline;
-module.exports.init = waterline.init;
